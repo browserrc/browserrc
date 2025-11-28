48c68c596fad718bc5afeefee57c5ff1a41124f6
Blake Smith <blake@mac.lan>
2025-11-27 22:03:09 -0800

    types

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+declare module 'browserrc' {
+  /**
+   * Returns a greeting message from browserrc
+   */
+  export function hello(): string;
+
+  /**
+   * The current version of browserrc
+   */
+  export const version: string;
+}
